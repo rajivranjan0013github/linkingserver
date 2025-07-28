ce866844611b0456c0de44ea8a9775ddd2595f5e
rajiv <rajivranjan0013@gmail.com>
2025-07-29 04:47:55 +0530

    init

---
+++ pages/questions/[id].js
@@ -0,0 +1,20 @@
+// pages/question/[id].js
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+export default function RedirectQuestion() {
+  const router = useRouter();
+  const { id } = router.query;
+
+  useEffect(() => {
+    if (id) {
+      const fallbackUrl = encodeURIComponent(`https://play.google.com/store/apps/details?id=com.question2&referrer=question=${id}`);
+      const intentUrl = `intent://question/${id}#Intent;scheme=https;package=com.question2;S.browser_fallback_url=${fallbackUrl};end`;
+
+      window.location.href = intentUrl;
+    }
+  }, [id]);
+
+  return <p>Redirecting to app or Play Store...</p>;
+}
+
