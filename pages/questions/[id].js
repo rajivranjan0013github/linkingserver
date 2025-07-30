d7810daacb2774eedf7ba55f9a71c972564a2737
rajiv <rajivranjan0013@gmail.com>
2025-07-30 22:55:06 +0530

    ggg

--- pages/questions/[id].js
+++ pages/questions/[id].js
@@ -10,10 +10,10 @@ export default function RedirectQuestion() {
       const fallbackUrl = encodeURIComponent(`https://play.google.com/store/apps/details?id=com.question2&referrer=question=${id}`);
       const intentUrl = `intent://questions/${id}#Intent;scheme=https;package=com.question2;S.browser_fallback_url=${fallbackUrl};end`;
 
-      // const timeout = setTimeout(() => {
-      //   // Fallback in case intent doesn't work
-      //   window.location.href = `https://play.google.com/store/apps/details?id=com.question2&referrer=question=${id}`;
-      // }, 2000);
+      const timeout = setTimeout(() => {
+        // Fallback in case intent doesn't work
+        window.location.href = `https://play.google.com/store/apps/details?id=com.question2&referrer=question=${id}`;
+      }, 2000);
 
       // Try to open the app
       window.location.href = intentUrl;
